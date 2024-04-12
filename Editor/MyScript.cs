17048028e4016dafac19c0665e6fed448f01d098
Samuel Ryberg <samuel@flashbulbgames.com>
2024-04-12 12:31:50 +0200

    test update

--- Editor/MyScript.cs
+++ Editor/MyScript.cs
@@ -5,12 +5,12 @@ public class MyScript : MonoBehaviour
 {
     private void Start()
     {
-        UnityEngine.Debug.Log("[SamTest] Start Hello World!");
+        UnityEngine.Debug.Log("[SamTest] Start Hello World! v2");
     }
 
     [MenuItem("SamTesting/Hello World")]
     static void Testing()
     {
-        UnityEngine.Debug.Log("[SamTest] MenuItem Hello World!");
+        UnityEngine.Debug.Log("[SamTest] MenuItem Hello World! v2");
     }
 }
